58a2ecdacb26ff04139e8a700b11e20dd93ca60f
ESACode <ESAWrite@gmail.com>
2021-07-12 22:18:51 -0400

    first commit

---
+++ Video-Game-Library-and-Wish-List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Video_Game_Library_and_Wish_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
