38dc423ef7b6452e5133d9420db30925196f281b
ESACode <ESAWrite@gmail.com>
2021-07-23 13:35:16 -0400

    Add/remove methods, and sorting methods

--- Video-Game-Library-and-Wish-List/Library.cs
+++ Video-Game-Library-and-Wish-List/Library.cs
@@ -17,12 +17,46 @@ namespace Video_Game_Library_and_Wish_List
         }
         public void Display(List<Game> GamesList)
         {
+            Console.WriteLine();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.WriteLine($"{this.NameOfList} \r\n");
             foreach(Game game in GamesList)
             {
-                Console.WriteLine($"Title: {game.Title}  Category: {game.Category}  System: {game.System}  Year: {game.Year}");
+                int place = GamesList.IndexOf(game) + 1;
+                Console.WriteLine($"{place}: Title: {game.Title}  Category: {game.Category}  System: {game.System}  Year: {game.Year}");
             }
             Console.WriteLine();
+        }
+
+        public void AddToList(Game game)
+        {
+            GamesList.Add(game);
+        }
+
+        public void RemoveFromList(int index)
+        {
+            GamesList.RemoveAt(index - 1);
+        }
+
+        public Library SortByYear(string name, List<Game> list)
+        {
+            List<Game> sortedList = list.OrderBy(item => item.Year).ToList();
+            Library sortedLibrary = new Library(name, sortedList);
+            return sortedLibrary;
+        }
+
+        public Library SortByTitle(string name, List<Game> list)
+        {
+            List<Game> sortedList = list.OrderBy(item => item.Title).ToList();
+            Library sortedLibrary = new Library(name, sortedList);
+            return sortedLibrary;
+        }
+
+        public Library SortBySystem(string name, List<Game> list)
+        {
+            List<Game> sortedList = list.OrderBy(item => item.System).ToList();
+            Library sortedLibrary = new Library(name, sortedList);
+            return sortedLibrary;
         }
     }
 }
--- Video-Game-Library-and-Wish-List/Program.cs
+++ Video-Game-Library-and-Wish-List/Program.cs
@@ -35,31 +35,126 @@ namespace Video_Game_Library_and_Wish_List
             var sampleLibrary = new Library("Eric's Sample Library", sampleList);
 
             //Read from the Json file
-            //var fileName = "RecommendGames.json";
-            List<WishListGame> RecommendGamesList = JsonConvert.DeserializeObject<List<WishListGame>>(File.ReadAllText("RecommendGames.json"));
-            var RecommendGamesLibrary = new WishList("Recommend Games List", RecommendGamesList);
-            RecommendGamesLibrary.Display(RecommendGamesLibrary.WishGamesList);
+            //List<WishListGame> RecommendGamesList = JsonConvert.DeserializeObject<List<WishListGame>>(File.ReadAllText("RecommendGames.json"));
+            //var RecommendGamesLibrary = new WishList("Recommend Games List", RecommendGamesList);
+            //RecommendGamesLibrary.Display(RecommendGamesLibrary.WishGamesList);
 
             //Master loop
             while (true)  
-            { 
+            {
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("Main Menu\r\n");
                 Console.WriteLine("Choose from the options below:\r\n");
                 Console.WriteLine("A: See a sample Video Game Library");
                 Console.WriteLine("B: Create your own Video Game Library");
                 Console.WriteLine("Q: Quit the program \r\n");
-                Console.WriteLine("Enter your selection (Type \"A\", \"B\", or \"C\", etc.) \r\n");
+                Console.WriteLine("Enter your selection (Type \"A\", \"B\", or \"C\", etc.)");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 ConsoleKeyInfo userInput = Console.ReadKey();
-                Console.WriteLine();
-                Console.WriteLine();
-                
+
+                //Quit the program
                 if (userInput.KeyChar == 'Q' || userInput.KeyChar == 'q')
                 {
-                    Console.WriteLine();
                     break;
                 }
+                //Show sample Library
                 else if (userInput.KeyChar == 'A' || userInput.KeyChar == 'a')
                 {
                     sampleLibrary.Display(sampleLibrary.GamesList);
+                    while (true)
+                    {
+                        Console.WriteLine("Choose from the options below:\r\n");
+                        Console.WriteLine("A: Add a game to the library");
+                        Console.WriteLine("B: Remove a game from the library");
+                        Console.WriteLine("C: Sort the list by year");
+                        Console.WriteLine("D: Sort the list by name");
+                        Console.WriteLine("E: Sort the list by system");
+                        Console.WriteLine("R: Return to main menu");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        ConsoleKeyInfo userInput1 = Console.ReadKey();
+
+                        //Add Game to Sample Library
+                        if (userInput1.KeyChar == 'A' || userInput1.KeyChar == 'a')
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        }
+                        //Remove Game from Sample Library
+                        else if (userInput1.KeyChar == 'B' || userInput1.KeyChar == 'b')
+                        {
+                            while (true)
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                                Console.WriteLine("Enter the game you want removed's numerical position in the list.\r\n");
+                                Console.WriteLine("(Type \"1\", \"2\", or \"3\", etc. and hit enter.  Enter \"B\" to go back to the previous menu");
+                                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                                string removeSelection = Console.ReadLine();
+                                if (removeSelection == "B" || removeSelection == "b")
+                                {
+                                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                                    break;
+                                }
+                                else
+                                {
+                                    int removeSelectionAsInt;
+                                    if (int.TryParse(removeSelection, out removeSelectionAsInt) && removeSelectionAsInt <= sampleLibrary.GamesList.Count() && removeSelectionAsInt > 0)
+                                    {
+                                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+                                        sampleLibrary.RemoveFromList(removeSelectionAsInt);
+                                        sampleLibrary.Display(sampleLibrary.GamesList);
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                                        Console.WriteLine("Your selection was either not a number, or the number did not match the position of any game in the library.");
+                                        Console.WriteLine("Try again.");
+                                        continue;
+                                    }
+                                }
+                            }
+                        }
+                        //Sort Sample Library by Year
+                        else if (userInput1.KeyChar == 'C' || userInput1.KeyChar == 'c')
+                        {
+                            var sortedLibrary = sampleLibrary.SortByYear("Eric's Sample Library: Sorted by Year", sampleLibrary.GamesList);
+                            sortedLibrary.Display(sortedLibrary.GamesList);
+                        }
+                        //Sort Sample Library by Title
+                        else if (userInput1.KeyChar == 'D' || userInput1.KeyChar == 'd')
+                        {
+                            var sortedLibrary = sampleLibrary.SortByTitle("Eric's Sample Library: Sorted by Title", sampleLibrary.GamesList);
+                            sortedLibrary.Display(sortedLibrary.GamesList);
+                        }
+                        //Sort Sample Library by System
+                        else if (userInput1.KeyChar == 'E' || userInput1.KeyChar == 'e')
+                        {
+                            var sortedLibrary = sampleLibrary.SortBySystem("Eric's Sample Library: Sorted by System", sampleLibrary.GamesList);
+                            sortedLibrary.Display(sortedLibrary.GamesList);
+                        }
+                        //Go back to Main Menu
+                        else if (userInput1.KeyChar == 'R' || userInput1.KeyChar == 'r')
+                        {
+                            Console.WriteLine("\r\n");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\r\n");
+                            Console.WriteLine("That was not a valid selction.  Please Try again.");
+                            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                            continue;
+                        }
+                    }
+                }
+                //Create new Library
+                else if (userInput.KeyChar == 'B' || userInput.KeyChar == 'b')
+                {
+                    Console.WriteLine("\r\n");
+                    Console.WriteLine("What is the name of your library?");
+                    var libraryName = Console.ReadLine();
                 }
                 else 
                 {
